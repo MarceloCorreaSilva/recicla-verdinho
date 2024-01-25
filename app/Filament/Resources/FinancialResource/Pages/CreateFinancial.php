dfbc515215bebcc6d35613a2c5ca552d9aaaa8af
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2024-01-25 18:23:55 -0300

    updates

--- app/Filament/Resources/FinancialResource/Pages/CreateFinancial.php
+++ app/Filament/Resources/FinancialResource/Pages/CreateFinancial.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateFinancial extends CreateRecord
 {
     protected static string $resource = FinancialResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/MovementResource/Pages/CreateMovement.php
+++ app/Filament/Resources/MovementResource/Pages/CreateMovement.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateMovement extends CreateRecord
 {
     protected static string $resource = MovementResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
